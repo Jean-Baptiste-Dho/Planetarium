f2e47db37b914d6697a47c05a6d0e6a0caeff5d3
jeanlebassiste <95588861+jeanlebassiste@users.noreply.github.com>
2022-12-22 16:56:38 +0100

    changement du menu pour ajout des liens

--- js/menu.js
+++ js/menu.js
@@ -1,25 +1,36 @@
-const titre = document.title
-
+const titre = document.title;
 
 const tabListPlanete = [
-    "Mercure",
-    "Vénus",
-    "Terre",
-    "Mars",
-    "Jupiter",
-    "Saturne",
-    "Uranus",
-    "Neptune",
-]
+  "Mercure",
+  "Vénus",
+  "Terre",
+  "Mars",
+  "Jupiter",
+  "Saturne",
+  "Uranus",
+  "Neptune",
+];
 
-const searchAndDestroy = (cible, listecible) => {
-    let position = tabListPlanete.indexOf(titre)
-    tabListPlanete.splice(position,1)
-    for (i=0; i<7; i++){
-        document.querySelector(`#item${i+1}`).innerHTML = tabListPlanete[i]
-    }
-}
+const hrefPlanete = [
+  "mercure.html",
+  "venus.html",
+  "terre.html",
+  "mars.html",
+  "jupiter.html",
+  "saturne.html",
+  "uranus.html",
+  "neptune.html",
+];
 
-searchAndDestroy(titre,tabListPlanete)
+const searchAndDestroy = (cible, listecible1, listecible2) => {
+  let position = listecible1.indexOf(cible);
+  listecible1.splice(position, 1);
+  for (i = 0; i < 7; i++) {
+    document.querySelector(`#item${i + 1}`).innerHTML = listecible1[i];
+  }
+  for (i = 0; i < 7; i++) {
+    document.querySelector(`#item${i + 1}`).href = listecible2[i];
+  }
+};
 
-
+searchAndDestroy(titre, tabListPlanete, hrefPlanete);
