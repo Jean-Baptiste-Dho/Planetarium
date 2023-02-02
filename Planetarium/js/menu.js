4c36bf09daa7b583ec00d31835a539cbfe6ae22c
Jean-Baptiste DHO <jeanbaptiste.dho.pro@gmail.com>
2023-02-02 14:46:15 +0100

    modification lien et footer

--- Planetarium/js/menu.js
+++ Planetarium/js/menu.js
@@ -2,35 +2,26 @@ const titre = document.title;
 
 const tabListPlanete = [
   "Mercure",
-  "Vénus",
+  "Venus",
   "Terre",
   "Mars",
   "Jupiter",
   "Saturne",
   "Uranus",
-  "Neptune",
+  "Neptune",  
 ];
 
 const hrefPlanete = [
-  "mercure.html",
-  "venus.html",
-  "terre.html",
-  "mars.html",
-  "jupiter.html",
-  "saturne.html",
-  "uranus.html",
-  "neptune.html",
+
 ];
 
-const searchAndDestroy = (cible, listecible1, listecible2) => {
+const searchAndDestroy = (cible, listecible1) => {
   let position = listecible1.indexOf(cible);
   listecible1.splice(position, 1);
   for (i = 0; i < 7; i++) {
     document.querySelector(`#item${i + 1}`).innerHTML = listecible1[i];
-  }
-  for (i = 0; i < 7; i++) {
-    document.querySelector(`#item${i + 1}`).href = listecible2[i];
+    document.querySelector(`#item${i + 1}`).href = `${listecible1[i]}.html`;
   }
 };
 
-searchAndDestroy(titre, tabListPlanete, hrefPlanete);
+searchAndDestroy(titre, tabListPlanete);
